9aeecb130ef20060483fcece41899289bea7dffa
Zachary Dodge <zacharysdodge@gmail.com>
2024-11-16 13:02:20 -0800

    feat: add some nvim plugins

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -6,8 +6,16 @@
 return {
   "AstroNvim/astrocommunity",
   -- import/override with your plugins folder
+  { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.diagnostics.error-lens-nvim" },
+  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+  { import = "astrocommunity.fuzzy-finder.fzf-lua" },
+  { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.motion.hop-nvim" },
   { import = "astrocommunity.pack.lean" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rainbow-delimiter-indent-blankline" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.scrolling.cinnamon-nvim" },
+  { import = "astrocommunity.scrolling.nvim-scrollbar" },
 }
