ad2939192ba160d43b68720b1369bf1b7cd26d3e
Zachary Dodge <zacharysdodge@gmail.com>
2026-03-19 17:15:08 -0700

    Setup ts_ls, eslint, and prettier automatically

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -603,7 +603,9 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        eslint = {},
+        prettier = {},
+        ts_ls = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -691,7 +693,10 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
